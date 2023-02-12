04656ec04347329fd5dd111c0d1cf387f63b4530
Yamen Alanbari <swd.yamen@gmail.com>
2023-02-12 18:14:30 +0000

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div style={{ margin: "30px" }} className="jumbotron text-center">
-      <h1>Welcome to Dogs lover gallery</h1>
+      <h1>Welcome to dogs lover gallery</h1>
     </div>
   );
 }
