593dbdefeeea0dfb74467fc38e029c14ad958ce6
Yamen Alanbari <swd.yamen@gmail.com>
2023-02-12 17:50:13 +0000

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div style={{ margin: "30px" }} className="jumbotron text-center">
+      <h1>Welcome to Dogs lover gallery</h1>
+    </div>
+  );
+}
+
+export default Header;
